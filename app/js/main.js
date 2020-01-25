01f475ed8d7c00ab3fbf32ccea88f4ef8875eb4d
Slavka <slavik.nukutuk@gmail.com>
2020-01-26 00:50:58 +0200

    fix scroll

--- app/js/main.js
+++ app/js/main.js
@@ -111,34 +111,34 @@ function getWeather(latitude, longitude){
                case 1: 
                   month = 'February';
                   break;
-               case 1: 
+               case 2: 
                   month = 'March';
                   break;
-               case 1: 
+               case 3: 
                   month = 'April';
                   break;
-               case 1: 
+               case 4: 
                   month = 'May';
                   break;
-               case 1: 
+               case 5: 
                   month = 'June';
                   break;
-               case 1: 
+               case 6: 
                   month = 'July';
                   break;
-               case 1: 
+               case 7: 
                   month = 'August';
                   break;
-               case 1: 
+               case 8: 
                   month = 'September';
                   break;
-               case 1: 
+               case 9: 
                   month = 'October';
                   break;
-               case 1: 
+               case 10: 
                   month = 'November';
                   break;
-               case 1: 
+               case 11: 
                   month = 'December';
                   break;
             }
